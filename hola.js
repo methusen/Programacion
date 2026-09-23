c5be50ae03a3e7e30a55ffa75c553aeae4d275bb
Daniel Cavallin <dcavallin@yahoo.com>
2026-09-23 02:01:11 -0400

    Agrega mensaje de aprendizaje de Git

--- hola.js
+++ hola.js
@@ -6,3 +6,4 @@ let edad = 55;
 console.log(chalk.green("Hola " + nombre));
 console.log(chalk.blue("Tienes " + edad + " años."));
 console.log(chalk.yellow("El año que viene tendrás " + (edad + 1) + " años."));
+console.log(chalk.magenta("Estoy aprendiendo Git."));
