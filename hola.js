47bf134355a97a83a552e8fb04ed80fa3b9cafd6
Daniel Cavallin <dcavallin@yahoo.com>
2026-09-23 01:40:16 -0400

    Primer programa con Node y npm

---
+++ hola.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+let nombre = "Daniel";
+let edad = 55;
+
+console.log(chalk.green("Hola " + nombre));
+console.log(chalk.blue("Tienes " + edad + " años."));
+console.log(chalk.yellow("El año que viene tendrás " + (edad + 1) + " años."));
